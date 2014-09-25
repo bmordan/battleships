32d5dfb8de5b56367719a33a6430eb4fab83469b
Bernard Mordan <bernardmordan@gmail.com>
2014-09-25 07:47:48 +0100

    deploy ship can pick a key that will not spill a ship

--- lib/deploy_ships.rb
+++ lib/deploy_ships.rb
@@ -4,4 +4,24 @@ module DeployShips
     ['horizontal','vertical'].sample
   end
 
+  def select_a_key
+    keys = []
+    ("A".."J").to_a.each {|a| (1..10).to_a.each {|n| keys << "#{a}#{n}"}}
+    keys.sample
+  end
+
+  def check_row_space(ship,key)
+    row = key[0]
+    col = key.slice(1..-1)
+    col.to_i < 10-ship.size ? true : false
+  end
+
+  def place_on_row(ship)
+    key = select_a_key
+    check_row_space(ship,key)
+
+  end
+
+
+
 end
--- spec/deploy_ships_spec.rb
+++ spec/deploy_ships_spec.rb
@@ -3,10 +3,21 @@ require 'deploy_ships'
 class Deploy; include DeployShips; end
 
 describe Deploy do
-  let(:deploy) {Deploy.new}
+  let(:deploy) { Deploy.new   }
+  let(:ship  ) { double :ship }
 
   it "should select a direction for a ship" do
     expect(deploy.direction).to satisfy {|value| value == 'horizontal' || value == 'vertical'}
   end
+
+  it "should pick a random key from the grid hash" do
+    expect(deploy.select_a_key).to match(/^[A-J]{1}\d{1,2}/)
+  end
+
+  it "should not allow a ship to spill outside a row" do
+    allow(ship).to receive(:size).and_return(4)
+    expect(deploy.check_row_space(ship,"C8")).to be false
+  end
+
 end
 
