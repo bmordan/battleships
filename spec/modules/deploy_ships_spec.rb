a285f13f68d720e2897d8cca0a383de3678485b1
Bernard Mordan <bernardmordan@gmail.com>
2014-09-26 19:05:28 +0100

    STABLE starting deploy ships overlapping logic

--- spec/modules/deploy_ships_spec.rb
+++ spec/modules/deploy_ships_spec.rb
@@ -1,15 +1,25 @@
-# require_relative '../../lib/modules/deploy_ships.rb'
+require_relative '../../lib/modules/deploy_ships.rb'
 
-# class Deploy; include DeployShips; end
+class Deploy; include DeployShips; end
 
-# describe Deploy do
+describe Deploy do
   
-#   let(:deploy) { Deploy.new            }
-#   let(:ship  ) { double :ship, location: [] }
-#   let(:ships ) { double :ships              }
+  let(:deploy    ) { Deploy.new                 }
+  let(:ship      ) { double :ship, location: [] }
+  let(:ships     ) { double :ships              }
+  let(:coordinate) { double :coordinate         }
+  let(:location  ) { double :location           }
 
-#   it "can use position_ship module" do
-#     allow(ships).to receive(:each)
-#     expect(deploy.place_ships(ships)).to eq(5)
-#   end
-# end
+  it "can check a ships location" do
+    allow(ship).to receive(:size).and_return(3)
+    expect(deploy.place_ship(ship)).to be_a(Array)
+  end
+
+  it "should check through each element in the array" do 
+    allow(ship).to receive(:size).and_return(3)
+    result = deploy.place_ship(ship)
+    expect( result.any? {|i| i.is_a?(String) }).to be true 
+  end
+
+
+end
